81dc8527542c28e474b268880f535204e4130fc7
Chi Chow <kyroschow@gmail.com>
2019-09-14 16:49:21 -0700

    Change navigation to functional component

--- src/navigation/Navigation.kt
+++ src/navigation/Navigation.kt
@@ -1,28 +1,24 @@
 package navigation
 
-import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
+import react.*
 import react.dom.div
 import react.dom.h3
 import react.dom.ul
 import react.router.dom.navLink
 
-class Navigation : RComponent<RProps, RState>() {
-    override fun RBuilder.render() {
-        div(classes = "nav") {
-            h3 { +"Logo" }
-            ul(classes = "nav-links") {
-                navLink("/about", className = "nav-style") {
-                    +"About"
-                }
-                navLink("/shop", className = "nav-style") {
-                    +"shop"
-                }
+fun RBuilder.navigation() = child(NAVIGATION)
+
+val NAVIGATION: FunctionalComponent<RProps> = functionalComponent {
+    div(classes = "nav") {
+        h3 { +"Logo" }
+        ul(classes = "nav-links") {
+            navLink("/about", className = "nav-style") {
+                +"About"
+            }
+            navLink("/shop", className = "nav-style") {
+                +"Shop"
             }
         }
     }
 }
 
-fun RBuilder.navigation() = child(Navigation::class) { }
